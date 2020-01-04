c94641afda0ecb1ade1e0e3edaa8ce20974df67f
umairx97 <umair.bajwa@hotmail.com>
2020-01-05 03:25:50 +0500

    moved data and add styles

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,47 +1,14 @@
 import React, { Component } from 'react'
 import './directory.styles.scss'
 import MenuItems from 'components/menu-items/menu-items.component'
-
+import { sections } from './data'
 
 class Directory extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'shop/hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens'
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens'
-        }
-      ]
+      sections
     }
   }
   render() {
