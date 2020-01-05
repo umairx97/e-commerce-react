d9e2b453643c9b18f2f6625bbc96a4e5923d4104
umairx97 <umair.bajwa@hotmail.com>
2020-01-05 22:23:40 +0500

    added transition and styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePage from 'pages/HomePage/HomePage.component';
+import './App.css'
 
 const App = () => (
   <div>
--- src/components/menu-items/menu-items.component.jsx
+++ src/components/menu-items/menu-items.component.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './menu-items.styles.scss'
 
 const MenuItems = ({ title, imageUrl, size }) => (
-  <div style={{
-    backgroundImage: `url(${imageUrl})`
-  }} className={`${size} menu-item`}>
+  <div className={`${size} menu-item`}>
+    <div style={{
+      backgroundImage: `url(${imageUrl})`
+    }} className="background-image" />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
